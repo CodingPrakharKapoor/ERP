bd4be28c7d4275bb3fb91592052f6429f1ec6f0b
khushbooag9 <khushiag565@gmail.com>
2024-11-27 10:15:05 +0530

    App.jsx & ERPMain.jsx added

--- client/src/ERPMain.jsx
+++ client/src/ERPMain.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
+import { Outlet, useParams } from 'react-router-dom'
+import axios from 'axios'
 import Navbar from './components/navbar/Navbar'
 import Sidebar from './components/sidebar/Sidebar'
 import Dashboard from './components/dashboard/Dashboard'
 
 function ERPMain() {
+  const {roll} = useParams();
+  const [studentData, setStudentData] = useState({});
+
+  const fetchStudentData = async () => {
+    try {
+      const response = await axios.get(`/api/student/${roll}`);
+      if (response.data) {
+        setStudentData(response.data);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect( () => {
+    fetchStudentData();
+  }, []);
+  
  return (
     <>
+        <Navbar studentData={studentData}/>
+        <div className="flex">
+        <Sidebar />
+        <div>
+          <Outlet/>
+        </div>
+      </div>
+
     </>
   )
 }
